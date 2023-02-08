ae2d10efebf361a50d44c9cf38422d5792d82104
Dilmad14 <dillion.14@hotmail.com>
2023-02-08 11:51:03 +0000

    Revert "Merge branch 'main' of https://github.com/TaiyeI/Group46"
    This reverts commit cfd25ecdabd11791f5024afe410f8957dadebed0, reversing
    changes made to c42ce7d1b14e3e69618c101b22e31228ddd87f18.

---
+++ energyaware/src/Pages/Tips.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Tips = () => {
+  return (
+    <div>
+      <h1>Tips</h1>
+      <h2>This is the community run tip section</h2>
+    </div>
+
+  );
+};
+
+export default Tips;
